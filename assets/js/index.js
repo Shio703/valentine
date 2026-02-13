2600203765b95b26cae313bf1402f57b34084de0
Shio703 <shiodjamurashvili26@gmail.com>
2026-02-13 15:07:15 +0400

    V1.0.0 core logical previous value preserv issue resoloved

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,38 +4,31 @@ let numberOfRejections = 0;
 const really = () => {
   // TODO: Show some kitty picture.
 };
+let prevPosition = { clientX: null, clientY: null };
 
 // maxSafeScreenWidth && height is 40% of screen's inner size.
 const maxSafeScreenWidth = (window.innerWidth / 100) * 40;
 const maxSafeScreenHeight = (window.innerHeight / 100) * 40;
 console.log(maxSafeScreenWidth, maxSafeScreenHeight);
 
-btnId.addEventListener("mouseenter", (event) => {
+btnId.addEventListener("click", (event) => {
   numberOfRejections++;
 
   if (numberOfRejections > 5) really();
-
-  let prevPosition = { screenX: null, screenY: null };
 
   // randomNumberX && Y is random number up to maxSafeScreen size.
   const randomNumberX = Math.round(Math.random() * maxSafeScreenWidth);
   const randomNumberY = Math.round(Math.random() * maxSafeScreenHeight);
 
-  // differenceInX && Y is difference between previous coordinates and currently generated.
-  const differenceInX = event.screenX - prevPosition.screenX;
-  const differenceInY = event.screenY - prevPosition.screenY;
-
-  console.log("diffX: ", differenceInX, "diffY: ", differenceInY);
-  prevPosition.screenX = event.screenX;
-  prevPosition.screenY = event.screenY;
-
-  // here i'am checking if newly generated number is greather at least with 100 than old one.
-  if (differenceInX > 100 && differenceInY > 100) {
+  // here i'am checking if newly generated numbers are not the previous.
+  if (event.clientX !== prevPosition.clientX) {
     btnId.style.transform = `translate(${randomNumberX}px, ${randomNumberY}px)`;
+    prevPosition.clientX = event.clientX;
+    prevPosition.clientY = event.clientY;
     return;
   }
 
-  btnId.style.transform = `translate(${prevPosition.screenX + 80}px, ${prevPosition.screenY + 80}px)`;
+  btnId.style.transform = `translate(${prevPosition.clientX + 70}px, ${prevPosition.clientY + 70}px)`;
 
   console.log(event);
   console.log(prevPosition);
