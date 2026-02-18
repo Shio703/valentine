2b4f1e0face6c35d871a13f1e0f474f590fe7e94
Shio703 <shiodjamurashvili26@gmail.com>
2026-02-19 00:53:48 +0400

    V1.0.2 meta info added && improved safe screen size calculations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 const btnId = document.getElementById("btn");
+if (window.innerWidth < 577) alert("Valentine703 is not properly working on devices with touch screen, yet.");
 
 let numberOfRejections = 0;
 const really = () => {
@@ -6,12 +7,12 @@ const really = () => {
 };
 let prevPosition = { clientX: null, clientY: null };
 
-// maxSafeScreenWidth && height is 40% of screen's inner size.
-const maxSafeScreenWidth = (window.innerWidth / 100) * 40;
-const maxSafeScreenHeight = (window.innerHeight / 100) * 40;
-console.log(maxSafeScreenWidth, maxSafeScreenHeight);
-
 btnId.addEventListener("mouseover", (event) => {
+  // maxSafeScreenWidth && height is 40% of screen's inner size.
+  const maxSafeScreenWidth = (window.innerWidth / 100) * 40;
+  const maxSafeScreenHeight = (window.innerHeight / 100) * 40;
+
+  console.log("40% of scren size:", maxSafeScreenWidth, maxSafeScreenHeight);
   numberOfRejections++;
 
   if (numberOfRejections > 5) really();
